6c82417207ed02304e41f6c748c5740028499747
mg-yamanedaiki <d.yamane@mountain-gorilla.co.jp>
2023-05-17 16:03:15 +0900

    fix

--- app/models/user.py
+++ app/models/user.py
@@ -8,7 +8,7 @@ class User(Base, TimestampMixin):
     __tablename__ = "users"
     __table_args__ = {"comment": "ユーザー"}
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=T ue)
     customer_id = Column(Integer, nullable=True, comment="顧客ID")
     is_admin = Column(Boolean, default=False, comment="管理者か")
     name = Column(String(50), nullable=False, comment="名前")
