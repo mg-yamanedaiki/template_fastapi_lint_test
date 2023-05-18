0e7a6c50cf7f370b097679fe2a8bcb4e9b9f9814
mg-yamanedaiki <d.yamane@mountain-gorilla.co.jp>
2023-05-18 11:44:36 +0900

    テストが成功

--- tests/app/test_example.py
+++ tests/app/test_example.py
@@ -6,4 +6,4 @@ from tests.testutil import logger  # noqa: F401
 
 def test_example(client: TestClient) -> None:
     res = client.get("/docs")
-    assert res.status_code != status.HTTP_200_OK
+    assert res.status_code == status.HTTP_200_OK
