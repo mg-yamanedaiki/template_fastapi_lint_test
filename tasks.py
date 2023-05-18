a78dc9aefa1a5e81aab4be74fa903a3c8baf68eb
mg-yamanedaiki <d.yamane@mountain-gorilla.co.jp>
2023-05-18 10:02:19 +0900

    fix

--- tasks.py
+++ tasks.py
@@ -146,4 +146,5 @@ def run(cmd: Union[str, list], out: bool = True) -> None:
         print(f"\033[32mrun cmd\033[0m: {inner_cmd}")
 
     result = subprocess.run(inner_cmd, shell=True)
-    print(result.returncode)
+    if result.returncode != 0:
+        raise RuntimeError('MyPy failed with exit code {}'.format(result.returncode))
